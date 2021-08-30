27265bd1f041f22e7dd548f22f0b4af5a06f14dc
Govind Reddy <govind.reddy@73strings.com>
2021-08-30 14:07:12 +0530

    updated the script file.

--- src/main/resources/db/migrations/05-rename-columns.sql
+++ src/main/resources/db/migrations/05-rename-columns.sql
@@ -1,26 +1,28 @@
 -- Discount Adjustments
-ALTER TABLE `discount_adjustment` RENAME COLUMN `medium` to `median`;
-ALTER TABLE `discount_adjustment` RENAME COLUMN `quarter1` to `quartile1`;
-ALTER TABLE `discount_adjustment` RENAME COLUMN `quarter3` to `quartile3`;
+ALTER TABLE `discount_adjustment` CHANGE COLUMN `medium`  `median` INT(11);
+
+
+ALTER TABLE `discount_adjustment` CHANGE COLUMN `quarter1`  `quartile1` INT(11) ;
+ALTER TABLE `discount_adjustment` CHANGE COLUMN `quarter3`  `quartile3` INT(11);
 
 -- Discount Rate Computation
-ALTER TABLE `discount_rate_computation` RENAME COLUMN `concluded_credit_spread_medium` to `concluded_credit_spread_median`;
-ALTER TABLE `discount_rate_computation` RENAME COLUMN `concluded_credit_spread_quarter1` to `concluded_credit_spread_quartile1`;
-ALTER TABLE `discount_rate_computation` RENAME COLUMN `concluded_credit_spread_quarter3` to `concluded_credit_spread_quartile3`;
+ALTER TABLE `discount_rate_computation` CHANGE COLUMN `concluded_credit_spread_medium`  `concluded_credit_spread_median` DOUBLE;
+ALTER TABLE `discount_rate_computation` CHANGE COLUMN `concluded_credit_spread_quarter1`  `concluded_credit_spread_quartile1` DOUBLE;
+ALTER TABLE `discount_rate_computation` CHANGE COLUMN `concluded_credit_spread_quarter3`  `concluded_credit_spread_quartile3` DOUBLE;
 
-ALTER TABLE `discount_rate_computation` RENAME COLUMN `risk_free_rate_medium` to `risk_free_rate_median`;
-ALTER TABLE `discount_rate_computation` RENAME COLUMN `risk_free_rate_quarter1` to `risk_free_rate_quartile1`;
-ALTER TABLE `discount_rate_computation` RENAME COLUMN `risk_free_rate_quarter3` to `risk_free_rate_quartile3`;
+ALTER TABLE `discount_rate_computation` CHANGE COLUMN `risk_free_rate_medium`  `risk_free_rate_median` DOUBLE;
+ALTER TABLE `discount_rate_computation` CHANGE COLUMN `risk_free_rate_quarter1`  `risk_free_rate_quartile1` DOUBLE;
+ALTER TABLE `discount_rate_computation` CHANGE COLUMN `risk_free_rate_quarter3`  `risk_free_rate_quartile3` DOUBLE;
 
-ALTER TABLE `discount_rate_computation` RENAME COLUMN `ytm_medium` to `ytm_median`;
-ALTER TABLE `discount_rate_computation` RENAME COLUMN `ytm_quarter1` to `ytm_quartile1`;
-ALTER TABLE `discount_rate_computation` RENAME COLUMN `ytm_quarter3` to `ytm_quartile3`;
+ALTER TABLE `discount_rate_computation` CHANGE COLUMN `ytm_medium`  `ytm_median` DOUBLE;
+ALTER TABLE `discount_rate_computation` CHANGE COLUMN `ytm_quarter1`  `ytm_quartile1` DOUBLE;
+ALTER TABLE `discount_rate_computation` CHANGE COLUMN `ytm_quarter3`  `ytm_quartile3` DOUBLE;
 
 -- Interest Undrawn Capital
-ALTER TABLE `interest_undrawn_capital` RENAME COLUMN `interest_undrawn_payment_percentage` to `interest_undrawn_percentage`;
+ALTER TABLE `interest_undrawn_capital` CHANGE COLUMN `interest_undrawn_payment_percentage`  `interest_undrawn_percentage` DOUBLE;
 
 -- Interim Payment Details
-ALTER TABLE `interim_payment_details` RENAME COLUMN `cutomizable_cashflow_excel` to `customizable_cashflow_excel`;
+ALTER TABLE `interim_payment_details` CHANGE COLUMN `cutomizable_cashflow_excel`  `customizable_cashflow_excel` BIGINT(20);
 
 -- Payment Schedule
-ALTER TABLE `payment_schedule` RENAME COLUMN `prepayment_details` to `repayment_details`;
+ALTER TABLE `payment_schedule` CHANGE COLUMN `prepayment_details`  `repayment_details` BIGINT(20);
