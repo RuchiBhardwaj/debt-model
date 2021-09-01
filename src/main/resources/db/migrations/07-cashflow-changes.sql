fac4911eddf3b838362fec6575cd418a0a6e726b
saravanan-blsk <saravanan@nineleaps.com>
2021-09-01 16:45:54 +0530

    Added DB migration script

---
+++ src/main/resources/db/migrations/07-cashflow-changes.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `cashflow_schedule` ADD `custom_cashflow_amount` DOUBLE NULL DEFAULT 0;
+ALTER TABLE `cashflow_schedule` ADD `custom_cashflow_name` VARCHAR(255) NULL DEFAULT 0;
+ALTER TABLE `cashflow_schedule` ADD `custom_cashflow_percentage` DOUBLE NULL DEFAULT 0;
